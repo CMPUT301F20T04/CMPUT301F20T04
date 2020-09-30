fbab13e9f7169150d0283d99a97f233044df2460
jordon-ogo <60081407+jordon-ogo@users.noreply.github.com>
2020-09-29 22:34:23 -0400

    Update Shape.java
    Removed unnecessary text that shoud have been removed before pushed to master

--- app/src/main/java/com/example/cmput301f20t04/Shape.java
+++ app/src/main/java/com/example/cmput301f20t04/Shape.java
@@ -3,12 +3,6 @@ package com.example.cmput301f20t04;
 public abstract class Shape {
     private int x, y;
     private String colour1 = "$PUT_YOUR_CHOICE_OF_COLOUR";
-<<<<<<< HEAD
     private String colour2 = "Green";
     private String colour3 = "Blue";
-=======
-    private String colour2 = "Red";
-    private String colour3 = "Blue";
-    private String colour4 = "Black";
->>>>>>> cb70a35d47b837a9e1437856332d6bb3f8023278
 }
