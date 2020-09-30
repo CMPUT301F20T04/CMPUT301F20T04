a0adcd20a666a5cf36621bf7a2e73a8d3272fddf
jordon-ogo <60081407+jordon-ogo@users.noreply.github.com>
2020-09-29 22:05:29 -0400

    Update Shape.java

--- app/src/main/java/com/example/cmput301f20t04/Shape.java
+++ app/src/main/java/com/example/cmput301f20t04/Shape.java
@@ -3,4 +3,5 @@ package com.example.cmput301f20t04;
 public abstract class Shape {
     private int x, y;
     private String colour = "$PUT_YOUR_CHOICE_OF_COLOUR";
+    private String colour = "Red";
 }
