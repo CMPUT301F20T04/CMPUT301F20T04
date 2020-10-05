96367678520c2be8f65d57bea42443edf3699398
Joshua Irwin <jnirwin91@gmail.com>
2020-10-05 10:18:00 -0600

    Changed colour in shape:

--- app/src/main/java/com/example/cmput301f20t04/Shape.java
+++ app/src/main/java/com/example/cmput301f20t04/Shape.java
@@ -2,7 +2,7 @@ package com.example.cmput301f20t04;
 
 public abstract class Shape {
     private int x, y;
-    private String colour1 = "$PUT_YOUR_CHOICE_OF_COLOUR";
+    private String colour1 = "Blue";
     private String colour2 = "Green";
     private String colour3 = "Blue";
 }
